68dec5faba04df19f62c6cda7b511b2f54003925
Tony Hagwall <hagwall86@live.se>
2023-10-27 09:27:00 +0200

    fix the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@
 export default function Footer() {
   return (
     <div>
-      <footer className="flex justify-around items-center bg-neutral-700">
+      <footer className="flex flex-col justify-around items-center bg-neutral-700">
         <p>This project is made with Next.js and Tailwind with Tinymce Editor</p>
         <p>@2023 by Hagge86@</p>
       </footer>
